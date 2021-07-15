5584ed5dabbcdc322eaed545fcb9d2e41f799843
AylaAsia-LeoLi <70555311+AylaAsia-LeoLi@users.noreply.github.com>
2021-07-15 11:10:14 +0800

    Update Interface.swift
    添加密文框firstresponder

--- Sources/MbSwiftUIFirstResponder/iOS/Interface.swift
+++ Sources/MbSwiftUIFirstResponder/iOS/Interface.swift
@@ -13,7 +13,12 @@ extension TextField {
             .background(MbFRHackView<V, UITextField>(id: id, firstResponder: firstResponder, resignableUserOperations: resignableUserOperations))
     }
 }
-
+extension SecureField {
+    public func firstResponder<V: Hashable>(id: V, firstResponder: Binding<V?>, resignableUserOperations: MbFirstResponder.TextField.ResignableUserOperations = .all) -> some View {
+        self
+            .background(MbFRHackView<V, UITextField>(id: id, firstResponder: firstResponder, resignableUserOperations: resignableUserOperations))
+    }
+}
 extension TextEditor {
     public func firstResponder<V: Hashable>(id: V, firstResponder: Binding<V?>, resignableUserOperations: MbFirstResponder.TextEditor.ResignableUserOperations = .all) -> some View {
         self
